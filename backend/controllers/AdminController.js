392083a96c4311bb72e3a93c4eac477632916bcd
Meet Patel <patelmeetbhai6333@gmail.com>
2026-03-06 11:46:49 +0530

    fix controller

--- backend/controllers/AdminController.js
+++ backend/controllers/AdminController.js
@@ -61,12 +61,14 @@ exports.sendOtp = async (req, res) => {
     admin.otpExpiry = Date.now() + 5 * 60 * 1000; // 5 minutes
     await admin.save();
 
-  const transporter = nodemailer.createTransport({
-  service: "gmail",
+ const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
-  }
+    user: process.env.EMAIL,
+    pass: process.env.PASS,
+  },
 });
 
    await transporter.sendMail({
