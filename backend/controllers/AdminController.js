bba4c380eb0ced4b7c4efa78a0f3dfbcfc270981
Meet Patel <patelmeetbhai6333@gmail.com>
2026-03-10 14:00:25 +0530

    smtp timeout fix

--- backend/controllers/AdminController.js
+++ backend/controllers/AdminController.js
@@ -10,9 +10,16 @@ const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
   secure: false,
+  requireTLS: true,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
+  },
+  connectionTimeout: 30000,
+  greetingTimeout: 30000,
+  socketTimeout: 30000,
+  tls: {
+    rejectUnauthorized: false
   }
 });
 
