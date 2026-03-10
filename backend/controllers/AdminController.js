3f32fb42d98410e6a67107034de53fdc2ea5ff19
Meet Patel <patelmeetbhai6333@gmail.com>
2026-03-10 14:06:13 +0530

    dd

--- backend/controllers/AdminController.js
+++ backend/controllers/AdminController.js
@@ -7,19 +7,10 @@ const nodemailer = require("nodemailer");
 /* ================= SMTP TRANSPORTER ================= */
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-  requireTLS: true,
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
-  },
-  connectionTimeout: 30000,
-  greetingTimeout: 30000,
-  socketTimeout: 30000,
-  tls: {
-    rejectUnauthorized: false
   }
 });
 
