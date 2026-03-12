6bc0a037703752d3cbf42aa21832f89be7478c9f
Meet Patel <patelmeetbhai6333@gmail.com>
2026-03-12 12:21:10 +0530

    dd

--- frantend/frantpage/my-app/src/pages/navbar.tsx
+++ frantend/frantpage/my-app/src/pages/navbar.tsx
@@ -109,7 +109,7 @@ function Navbar() {
 
             <li className="nav-item ms-3">
               <Link 
-                to="/login"
+                to="https://student-management-system-1frantuser.onrender.com"
                 className="btn px-4 py-2 fw-bold"
                 style={{ 
                   backgroundColor: "rgba(255,255,255,0.2)",
